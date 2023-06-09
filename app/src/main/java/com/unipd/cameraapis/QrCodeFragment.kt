6e517fe36dd3b9dd2b2b0739181266b63c81f623
Nicola <nickbusato0101@gmail.com>
2023-06-09 12:37:20 +0200

    QrCodePopUp

---
+++ app/src/main/java/com/unipd/cameraapis/QrCodeFragment.kt
@@ -0,0 +1,21 @@
+package com.unipd.cameraapis
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+
+
+class QrCodeFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_qrcode, container, false)
+    }
+
+}
