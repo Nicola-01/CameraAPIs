f34bd70a4bc37717f04684294f5c9a579ac0392d
Nicola <nickbusato0101@gmail.com>
2023-06-12 16:13:59 +0200

    aggiunta verifica se il QRcode è di un wifi, così posso copiare la pw

--- app/src/main/java/com/unipd/cameraapis/QrCodeFragment.kt
+++ app/src/main/java/com/unipd/cameraapis/QrCodeFragment.kt
@@ -8,7 +8,9 @@ import android.content.pm.ActivityInfo
 import android.graphics.Color
 import android.graphics.drawable.ColorDrawable
 import android.net.Uri
+import android.net.wifi.WifiManager
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.OrientationEventListener
 import android.view.View
@@ -16,8 +18,14 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.DialogFragment
+import java.util.regex.Pattern
 
 class QrCodeFragment : DialogFragment() {
+
+    val LINK = 0
+    val WIFI = 1
+    val TEXT = 2
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -35,15 +43,46 @@ class QrCodeFragment : DialogFragment() {
         dialog!!.window?.attributes?.screenOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
 
         url = arguments?.getString("URL")!!
-        val isLink = (url.startsWith("http://") || url.startsWith("https://"))
 
-        view.findViewById<TextView>(R.id.TV_linkQR).text = url
-
+        val textURL = view.findViewById<TextView>(R.id.TV_linkQR)
         val btOpen = view.findViewById<Button>(R.id.BT_openQrCodePopUp)
         val btCopy = view.findViewById<Button>(R.id.BT_copyQrCodePopUp)
 
-        if(!isLink) // cambio la grafica nel caso sia solo testo
+        textURL.text = url
+
+        var type = LINK
+        var ssid = ""
+        var password = ""
+
+        // in base al tipo di qrCode faccio cose diverse
+        if((url.startsWith("http://") || url.startsWith("https://"))) // e' un link
+            ;
+        else if(url.startsWith("WIFI:")) // salvataggio dati wifi
         {
+            type = WIFI
+            btCopy.text = "Copia Password"
+            btOpen.text = "Apri impostazioni"
+
+            // estraggo l'ssid e la password
+            val ssidPattern = Pattern.compile("S:([^;]+);")
+            val passwordPattern = Pattern.compile("P:([^;]+);")
+
+            val ssidMatcher = ssidPattern.matcher(url)
+            val passwordMatcher = passwordPattern.matcher(url)
+
+            ssidMatcher.find()
+            passwordMatcher.find()
+
+            ssid = ssidMatcher.group(1)?: ""            // se è null mette stringa vuota
+            password = passwordMatcher.group(1)?: ""    // se è null mette stringa vuota
+
+            textURL.text = "Nome WiFi: $ssid\n Password: $password"
+            btCopy.backgroundTintList = context?.getColorStateList(R.color.caribbean_Current)
+            btOpen.backgroundTintList = context?.getColorStateList(R.color.light_gray) // do meno importanza, prima va cliccato l' altro
+        }
+        else // è testo o qualcosa che non è stato implementato
+        {
+            type = TEXT
             btCopy.text = "Copia Testo"
             btOpen.text = "Apri" // è disattivato ma non mi piace che ci sia scritto link comunque
             btOpen.isEnabled = false
@@ -53,16 +92,19 @@ class QrCodeFragment : DialogFragment() {
             btCopy.backgroundTintList = context?.getColorStateList(R.color.caribbean_Current)
         }
 
-
-
         btOpen.setOnClickListener{
-            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-            startActivity(browserIntent)
+            val intent =
+                if(type == WIFI)
+                    Intent(Settings.ACTION_WIFI_SETTINGS)
+                else
+                    Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
         }
 
         btCopy.setOnClickListener {
             val clipboard = context?.getSystemService(CLIPBOARD_SERVICE) as ClipboardManager
-            val clip = ClipData.newPlainText("URL", url)
+            var clip = ClipData.newPlainText("URL", if(type == WIFI) password else url)
+            //se è un qr code di un wifi allora copio solo la password, altrimenti tutto il teso
             clipboard.setPrimaryClip(clip)
         }
 
